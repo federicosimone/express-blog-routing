9edc40515550fbdfcae0f4dd1c1ffedd72209691
federicosimone <federico.simone15@gmail.com>
2026-02-21 16:35:07 +0100

    patch and delete

--- routers/posts.js
+++ routers/posts.js
@@ -29,7 +29,15 @@ router.put('/:id', (req, res) => {
     res.send(`Stai chiedendo di modificare l'intero post con id: ${req.params.id}`)
 })
 
+router.patch('/:id', (req, res) => {
+    res.send(`Stai chiedendo di modificare parte del post con id: ${req.params.id}`)
+})
+
 //cruD - Delete
+
+router.delete('/:id', (req, res) => {
+    res.send(` Sei sicuro di voler eliminare il post con id: ${req.params.id}? `)
+})
 
 
 
