eb41092fa42886ad547f5b36eb28f104b83b4af9
federicosimone <federico.simone15@gmail.com>
2026-02-23 20:21:45 +0100

    changes data

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -5,7 +5,21 @@ function index(req, res) {
 }
 
 function show(req, res) {
-    res.send(`Stai visualizzando il post: ${req.params.id}`)
+
+    const id = Number(req.params.id);
+
+    if (id < 0) {
+        return res.status(400).json({ error: "Error", message: "ID non valido" });
+    }
+
+    const result = posts.find(post => post.id == id);
+
+    if (!result) {
+        return res.status(404).json({ error: "Not Found", message: "Post non trovato" });
+    }
+
+    return res.json(result);
+
 }
 
 function destroy(req, res) {
