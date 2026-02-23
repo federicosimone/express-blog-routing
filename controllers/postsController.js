a0de9795f9bc7bbc8589c5228d7819b02f2cb01c
federicosimone <federico.simone15@gmail.com>
2026-02-23 21:56:11 +0100

    destroy

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -23,7 +23,23 @@ function show(req, res) {
 }
 
 function destroy(req, res) {
-    res.send(` Sei sicuro di voler eliminare il post con id: ${req.params.id}? `)
+    const id = Number(req.params.id);
+
+    if (id < 0) {
+        return res.status(400).json({ error: "Error", message: "ID non valido" });
+    }
+
+    const result = posts.find(post => post.id == id);
+
+    if (!result) {
+        return res.status(404).json({ error: "Not Found", message: "Post non trovato" });
+    }
+
+    posts.splice(posts.indexOf(result), 1)
+
+    console.log(posts);
+
+    return res.sendStatus(204);
 }
 
 const funzioni = {
